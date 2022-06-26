38732f118f679a894ad21cf74fe0e3fccb789b5d
Him5696 <himanshi1252@gmail.com>
2022-06-26 21:32:47 +0530

    Added: Initial position

--- src/com/bridgelabz/snakeAndLadder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeAndLadder/SnakeAndLadder.java
@@ -3,5 +3,7 @@ package com.bridgelabz.snakeAndLadder;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game");
+        int position = 0;
+        System.out.println("game played at start position:" + position);
     }
 }
