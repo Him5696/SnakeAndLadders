0fe7fdece0a30743b5c69e5750ebc6468343634d
Him5696 <himanshi1252@gmail.com>
2022-06-26 21:39:51 +0530

    Rolling dice between 1 to 6

--- src/com/bridgelabz/snakeAndLadder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeAndLadder/SnakeAndLadder.java
@@ -1,9 +1,19 @@
 package com.bridgelabz.snakeAndLadder;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
+
+        private static int getDieRoll() {
+            Random random = new Random();
+            int roll = random.nextInt(6) + 1;
+            System.out.println(roll);
+            return roll;
+        }
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game");
         int position = 0;
         System.out.println("game played at start position:" + position);
+        int dieNumber = getDieRoll();
     }
 }
